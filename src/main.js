86d42afcc51fbc5b4cbf2e68065ee58d831cc0df
Aleksandr Zh <zhuravlev.a.a@me.com>
2019-12-11 17:51:53 +0300

    добавляет строку что нет фильмов в базе если массив фильмов пуст

--- src/main.js
+++ src/main.js
@@ -2,14 +2,12 @@ import ProfileStatusComponent from './components/profile-status.js';
 import FilterComponent from './components/filter.js';
 import SortingComponent from './components/sorting.js';
 import FilmListComponent from './components/film-list.js';
+import FilmListTitleComponent from './components/film-list-title.js';
 import FilmCardComponent from './components/film-card.js';
 import ShowMoreButtonComponent from './components/show-more-button.js';
 import ExtraListComponent from './components/extra-list.js';
 import FilmPopupComponent from './components/film-popup.js';
-import {
-  generateFilters,
-  generateFilmCardsData
-} from './mock.js';
+import {generateFilters, generateFilmCardsData} from './mock.js';
 
 const ExtraTitles = {
   TOP_RATED: `Top rated`,
@@ -37,10 +35,6 @@ const Count = {
 const KeyCode = {
   ESC: 27
 };
-
-let popup;
-let filmListsExtra;
-let closePopupBtn;
 
 const cardsData = generateFilmCardsData(Count.CARD);
 
@@ -73,11 +67,11 @@ const createFilmCardFragment = (data) => {
     const filmTitle = filmCardElement.getElement().querySelector(`h3`);
     const filmImage = filmCardElement.getElement().querySelector(`img`);
     const filmComments = filmCardElement.getElement().querySelector(`a`);
+    let closePopupBtn;
 
     const getCardClickHandler = (evt) => {
       if ([filmTitle, filmImage, filmComments].includes(evt.target)) {
         renderHtmlPart(Nodes.BODY, filmPopupElement.getElement(), RenderPosition.BEFOREEND);
-        popup = document.querySelector(`.film-details`);
         closePopupBtn = filmPopupElement.getElement().querySelector(`.film-details__close-btn`);
         closePopupBtn.addEventListener(`click`, removePopupCkickHandler);
         document.addEventListener(`keydown`, removePopupKeydownHandler);
@@ -102,6 +96,7 @@ const createFilmCardFragment = (data) => {
         popupRemove();
       }
     };
+
     filmCardElement.getElement().addEventListener(`click`, getCardClickHandler);
 
     fragment.appendChild(filmCardElement.getElement());
@@ -128,7 +123,8 @@ const renderFilmListExtra = (node) => {
   const isFilmsUnRated = ratingSortedFilms.every((film) => film.rating === 0);
   const isFilmsUnComment = ratingSortedFilms.every((comment) => comment.comments === 0);
 
-  filmListsExtra = node.querySelectorAll(`.films-list--extra`);
+  const filmListsExtra = node.querySelectorAll(`.films-list--extra`);
+
   if (isFilmsUnRated) {
     filmListsExtra[0].remove();
   } else {
@@ -166,24 +162,27 @@ const createFragment = (arr) => {
 };
 
 const pasteElements = () => {
+  const filmListComponent = new FilmListComponent(cardsData).getElement();
   renderHtmlPart(Nodes.HEADER, new ProfileStatusComponent(cardsData.length).getElement(), RenderPosition.BEFOREEND);
-  renderHtmlPart(Nodes.MAIN, createFragment([new FilterComponent(filters).getElement(), new SortingComponent().getElement(), new FilmListComponent().getElement()]), RenderPosition.BEFOREEND);
+  renderHtmlPart(Nodes.MAIN, createFragment([new FilterComponent(filters).getElement(), new SortingComponent().getElement(), filmListComponent]), RenderPosition.BEFOREEND);
+  renderHtmlPart(filmListComponent.querySelector(`.films-list`), new FilmListTitleComponent(cardsData).getElement(), RenderPosition.AFTERBEGIN);
 
-  const filmsContainer = Nodes.MAIN.querySelector(`.films`);
-  const filmsList = Nodes.MAIN.querySelector(`.films-list`);
-  const filmsListContainer = Nodes.MAIN.querySelector(`.films-list__container`);
+  if (cardsData.length > 0) {
+    const filmsContainer = Nodes.MAIN.querySelector(`.films`);
+    const filmsList = Nodes.MAIN.querySelector(`.films-list`);
+    const filmsListContainer = Nodes.MAIN.querySelector(`.films-list__container`);
+    const cardsOnStart = cardsData.slice(0, Count.SHOWING_CARDS_ON_START);
 
-  const cardsOnStart = cardsData.slice(0, Count.SHOWING_CARDS_ON_START);
+    renderHtmlPart(filmsListContainer, createFilmCardFragment(cardsOnStart), RenderPosition.BEFOREEND);
 
-  renderHtmlPart(filmsListContainer, createFilmCardFragment(cardsOnStart), RenderPosition.BEFOREEND);
+    renderHtmlPart(filmsList, new ShowMoreButtonComponent().getElement(), RenderPosition.BEFOREEND);
+    renderHtmlPart(filmsContainer, createFragment([new ExtraListComponent(ExtraTitles.TOP_RATED).getElement(), new ExtraListComponent(ExtraTitles.MOST_COMMENTED).getElement()]), RenderPosition.BEFOREEND);
+    renderFilmListExtra(filmsContainer);
 
-  renderHtmlPart(filmsList, new ShowMoreButtonComponent().getElement(), RenderPosition.BEFOREEND);
-  renderHtmlPart(filmsContainer, createFragment([new ExtraListComponent(ExtraTitles.TOP_RATED).getElement(), new ExtraListComponent(ExtraTitles.MOST_COMMENTED).getElement()]), RenderPosition.BEFOREEND);
-  renderFilmListExtra(filmsContainer);
+    const loadMoreButton = filmsList.querySelector(`.films-list__show-more`);
 
-  const loadMoreButton = filmsList.querySelector(`.films-list__show-more`);
-
-  loadMoreButton.addEventListener(`click`, loadMoreButtonClickHandler(filmsListContainer, loadMoreButton));
+    loadMoreButton.addEventListener(`click`, loadMoreButtonClickHandler(filmsListContainer, loadMoreButton));
+  }
   Nodes.FOOTER_STATISTIC.textContent = `${cardsData.length} movies inside`;
 };
 

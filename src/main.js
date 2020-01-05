f87b23e8577d06c065a09eed0d2a214614e57f95
Aleksandr <zhuravlev.a.a@me.com>
2020-01-05 20:47:21 +0300

    добавляет использование модели в main.js

--- src/main.js
+++ src/main.js
@@ -3,19 +3,21 @@ import {renderHtmlPart} from './utils/render.js';
 import ProfileStatusComponent from './components/profile-status.js';
 import FilmListComponent from './components/film-list.js';
 import FilmListTitleComponent from './components/film-list-title.js';
+import MoviesModel from './models/movies.js';
 import {generateFilmCardsData} from './mock.js';
 import PageController from './controllers/page-controller.js';
 
 const cardsData = generateFilmCardsData(Count.CARD);
-
+const moviesModel = new MoviesModel();
+moviesModel.setMovies(cardsData);
 
 const pasteElements = () => {
   const filmListComponent = new FilmListComponent(cardsData).getElement();
   renderHtmlPart(Nodes.HEADER, new ProfileStatusComponent(cardsData.length).getElement(), RenderPosition.BEFOREEND);
   renderHtmlPart(filmListComponent.querySelector(`.films-list`), new FilmListTitleComponent(cardsData).getElement(), RenderPosition.AFTERBEGIN);
 
-  const pageController = new PageController(filmListComponent);
-  pageController.render(cardsData);
+  const pageController = new PageController(filmListComponent, moviesModel);
+  pageController.render();
 
   Nodes.FOOTER_STATISTIC.textContent = `${cardsData.length} movies inside`;
 };

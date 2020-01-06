01cbc491507624a6575adaada5e5a1c2fdc682c5
Aleksandr <zhuravlev.a.a@me.com>
2020-01-06 04:21:38 +0300

    создает модуль успомогательных функций для фильтров

---
+++ src/utils/filter.js
@@ -0,0 +1,27 @@
+import {FilterName} from '../constants.js';
+
+const getWatchlistFilms = (films) => {
+  return films.filter((film) => film.isWatchList);
+};
+
+const getWatchedFilms = (films) => {
+  return films.filter((film) => film.isWatched);
+};
+
+const getFavoriteFilms = (films) => {
+  return films.filter((film) => film.isFavorite);
+};
+
+const getFilmsByFilter = (films, filterName) => {
+  switch (filterName) {
+    case FilterName.WATCHLIST:
+      return getWatchlistFilms(films);
+    case FilterName.WATCHED:
+      return getWatchedFilms(films);
+    case FilterName.FAVORITES:
+      return getFavoriteFilms(films);
+  }
+  return films;
+};
+
+export {getFilmsByFilter};

b527dcfb5a71e98734d0699191956da05c9db554
Aleksandr <zhuravlev.a.a@me.com>
2020-01-06 04:19:18 +0300

    добавляет контроллер фильтров в main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import {Nodes, Count, RenderPosition} from './constants.js';
 import {renderHtmlPart} from './utils/render.js';
 import ProfileStatusComponent from './components/profile-status.js';
+import FilterController from './controllers/filter.js';
+
 import FilmListComponent from './components/film-list.js';
 import FilmListTitleComponent from './components/film-list-title.js';
 import MoviesModel from './models/movies.js';
@@ -14,6 +16,9 @@ moviesModel.setMovies(cardsData);
 const pasteElements = () => {
   const filmListComponent = new FilmListComponent(cardsData).getElement();
   renderHtmlPart(Nodes.HEADER, new ProfileStatusComponent(cardsData.length).getElement(), RenderPosition.BEFOREEND);
+  const filterController = new FilterController(Nodes.MAIN, moviesModel);
+  filterController.render();
+
   renderHtmlPart(filmListComponent.querySelector(`.films-list`), new FilmListTitleComponent(cardsData).getElement(), RenderPosition.AFTERBEGIN);
 
   const pageController = new PageController(filmListComponent, moviesModel);

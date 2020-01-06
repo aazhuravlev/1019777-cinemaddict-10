fcad9a2e3b8db48de2a3f06b6556f337e2fc6076
Aleksandr <zhuravlev.a.a@me.com>
2020-01-06 04:18:16 +0300

    создает контроллер для фильтров

---
+++ src/controllers/filter.js
@@ -0,0 +1,51 @@
+import FilterComponent from '../components/filter.js';
+import {FilterName} from '../constants.js';
+import {renderHtmlPart, RenderPosition, replace} from '../utils/render.js';
+import {getFilmsByFilter} from '../utils/filter.js';
+
+export default class FilterController {
+  constructor(container, movieModel) {
+    this._container = container;
+    this._movieModel = movieModel;
+
+    this._activeFilterName = FilterName.ALL;
+    this._filterComponent = null;
+
+    this._onDataChange = this._onDataChange.bind(this);
+    this._onFilterChange = this._onFilterChange.bind(this);
+
+    this._movieModel.setDataChangeHandler(this._onDataChange);
+  }
+
+  render() {
+    const allFilms = this._movieModel.getMoviesAll();
+    const filters = Object.entries(FilterName).map(([link, filterName]) => {
+      return {
+        name: filterName,
+        link,
+        count: getFilmsByFilter(allFilms, filterName).length,
+        isActive: filterName === this._activeFilterName,
+      };
+    });
+
+    const oldComponent = this._filterComponent;
+    this._filterComponent = new FilterComponent(filters);
+
+    this._filterComponent.setFilterChangeHandler(this._onFilterChange);
+
+    if (oldComponent) {
+      replace(this._filterComponent, oldComponent);
+    } else {
+      renderHtmlPart(this._container, this._filterComponent.getElement(), RenderPosition.BEFOREEND);
+    }
+  }
+
+  _onFilterChange(filterName) {
+    this._movieModel.setFilter(filterName);
+    this._activeFilterName = filterName;
+  }
+
+  _onDataChange() {
+    this.render();
+  }
+}

8e69fcdc3b7ae56a3d44fe986032b2a78a192055
Aleksandr <zhuravlev.a.a@me.com>
2020-01-08 02:48:59 +0300

    создает обработчик клика по кнопке статистики и добавляет компонент статистики в main.js

--- src/main.js
+++ src/main.js
@@ -4,24 +4,42 @@ import ProfileStatusComponent from './components/profile-status.js';
 import FilterController from './controllers/filter.js';
 import FilmListComponent from './components/film-list.js';
 import FilmListTitleComponent from './components/film-list-title.js';
+import StatisticsComponent from './components/statistics.js';
 import MoviesModel from './models/movies.js';
 import {generateFilmCardsData} from './mock.js';
 import PageController from './controllers/page-controller.js';
+
+const showStatisticHandler = (pageController, statisticsComponent) => {
+  return (evt) => {
+    if (evt.target.className.includes(`main-navigation__item--additional`)) {
+      pageController.hide();
+      statisticsComponent.show();
+    } else {
+      pageController.show();
+      statisticsComponent.hide();
+    }
+  };
+};
 
 const cardsData = generateFilmCardsData(Count.CARD);
 const moviesModel = new MoviesModel();
 moviesModel.setMovies(cardsData);
 
 const pasteElements = () => {
-  const filmListComponent = new FilmListComponent(cardsData).getElement();
+  const filmListComponent = new FilmListComponent(cardsData);
+  const pageController = new PageController(filmListComponent, moviesModel);
+  const statisticsComponent = new StatisticsComponent();
+
   renderHtmlPart(Nodes.HEADER, new ProfileStatusComponent(cardsData.length).getElement(), RenderPosition.BEFOREEND);
-  const filterController = new FilterController(Nodes.MAIN, moviesModel);
+
+  const filterController = new FilterController(Nodes.MAIN, moviesModel, showStatisticHandler(pageController, statisticsComponent));
   filterController.render();
 
-  renderHtmlPart(filmListComponent.querySelector(`.films-list`), new FilmListTitleComponent(cardsData).getElement(), RenderPosition.AFTERBEGIN);
+  renderHtmlPart(filmListComponent.getElement().querySelector(`.films-list`), new FilmListTitleComponent(cardsData).getElement(), RenderPosition.AFTERBEGIN);
 
-  const pageController = new PageController(filmListComponent, moviesModel);
   pageController.render();
+  renderHtmlPart(Nodes.MAIN, statisticsComponent.getElement(), RenderPosition.BEFOREEND);
+  statisticsComponent.hide();
 
   Nodes.FOOTER_STATISTIC.textContent = `${cardsData.length} movies inside`;
 };
